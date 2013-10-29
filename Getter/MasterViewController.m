c43d5aa91e670d71cb3dfa4d6c43d2b539d6d509
sisiogen <e095709@ootsuboyuuki-no-MacBook.local>
2013-10-29 15:55:57 +0900

    test

--- Getter/MasterViewController.m
+++ Getter/MasterViewController.m
@@ -6,31 +6,60 @@
 //  Copyright (c) 2013年 大坪裕樹. All rights reserved.
 //
 
-#import "MasterViewController.h"
-
+//#import "MasterViewController.h"
 #import "DetailViewController.h"
+#import "TweetViewController.h"
 
-@interface MasterViewController () {
-    NSMutableArray *_objects;
-}
+@interface MasterViewController : UITableViewController <UIAlertViewDelegate, TweetViewControllerDelegate>
 @end
 
-@implementation MasterViewController
+
+
+#import "GTMOAuthAuthentication.h"
+#import "GTMOAuthViewControllerTouch.h"
+
+@implementation MasterViewController {
+    // OAuth認証オブジェクト
+    GTMOAuthAuthentication *auth_;
+    // 表示中ツイート情報
+    NSArray *timelineStatuses_;
+}
 
 - (void)awakeFromNib
 {
     [super awakeFromNib];
 }
 
+// KeyChain登録サービス名
+static NSString *const kKeychainAppServiceName = @"KodawariButter";
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
+    
+    // GTMOAuthAuthenticationインスタンス生成
+    // ※自分の登録アプリの Consumer Key と Consumer Secret に書き換えてください
+    NSString *consumerKey = @"AlYNIai1ijrgUUmlbfaxg";
+    NSString *consumerSecret = @"DeUNpwTEhn0FpuoRQKAwLF7O1dzjvgWEpT2zZhrPc";
+    auth_ = [[GTMOAuthAuthentication alloc]
+             initWithSignatureMethod:kGTMOAuthSignatureMethodHMAC_SHA1
+             consumerKey:consumerKey
+             privateKey:consumerSecret];
+    
+    // 既にOAuth認証済みであればKeyChainから認証情報を読み込む
+    BOOL authorized = [GTMOAuthViewControllerTouch
+                       authorizeFromKeychainForName:kKeychainAppServiceName
+                       authentication:auth_];
+    if (authorized) {
+        // 認証済みの場合はタイムライン更新
+        [self asyncShowHomeTimeline];
+    } else {
+        // 未認証の場合は認証処理を実施
+        [self asyncSignIn];
+    }
 }
+
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -38,14 +67,122 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)insertNewObject:(id)sender
+// 認証処理
+- (void)asyncSignIn
 {
-    if (!_objects) {
-        _objects = [[NSMutableArray alloc] init];
+    NSString *requestTokenURL = @"https://api.twitter.com/oauth/request_token";
+    NSString *accessTokenURL = @"https://api.twitter.com/oauth/access_token";
+    NSString *authorizeURL = @"https://api.twitter.com/oauth/authorize";
+    
+    NSString *keychainAppServiceName = @"KodawariButter";
+    
+    auth_.serviceProvider = @"Twitter";
+    auth_.callback = @"http://www.example.com/OAuthCallback";
+    
+    GTMOAuthViewControllerTouch *viewController;
+    viewController = [[GTMOAuthViewControllerTouch alloc]
+                      initWithScope:nil
+                      language:nil
+                      requestTokenURL:[NSURL URLWithString:requestTokenURL]
+                      authorizeTokenURL:[NSURL URLWithString:authorizeURL]
+                      accessTokenURL:[NSURL URLWithString:accessTokenURL]
+                      authentication:auth_
+                      appServiceName:keychainAppServiceName
+                      delegate:self
+                      finishedSelector:@selector(authViewContoller:finishWithAuth:error:)];
+    
+    [[self navigationController] pushViewController:viewController animated:YES];
+}
+
+// 認証エラー表示AlertViewタグ
+static const int kMyAlertViewTagAuthenticationError = 1;
+
+// 認証処理が完了した場合の処理
+- (void)authViewContoller:(GTMOAuthViewControllerTouch *)viewContoller
+           finishWithAuth:(GTMOAuthAuthentication *)auth
+                    error:(NSError *)error
+{
+    if (error != nil) {
+        // 認証失敗
+        NSLog(@"Authentication error: %d.", error.code);
+        UIAlertView *alertView;
+        alertView = [[UIAlertView alloc] initWithTitle:@"Error"
+                                               message:@"Authentication failed."
+                                              delegate:self
+                                     cancelButtonTitle:@"Confirm"
+                                     otherButtonTitles:nil];
+        alertView.tag = kMyAlertViewTagAuthenticationError;
+        [alertView show];
+    } else {
+        // 認証成功
+        NSLog(@"Authentication succeeded.");
+        // タイムライン表示
+        [self asyncShowHomeTimeline];
     }
-    [_objects insertObject:[NSDate date] atIndex:0];
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+}
+
+// UIAlertViewが閉じられた時
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    // 認証失敗通知AlertViewが閉じられた場合
+    if (alertView.tag == kMyAlertViewTagAuthenticationError) {
+        // 再度認証
+        [self asyncSignIn];
+    }
+}
+
+// デフォルトのタイムライン処理表示
+- (void)asyncShowHomeTimeline
+{
+    [self fetchGetHomeTimeline];
+}
+
+// タイムライン (home_timeline) 取得
+- (void)fetchGetHomeTimeline
+{
+    // 要求を準備
+    NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1.1/statuses/home_timeline.json"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod:@"GET"];
+    
+    // 要求に署名情報を付加
+    [auth_ authorizeRequest:request];
+    
+    // 非同期通信による取得開始
+    GTMHTTPFetcher *fetcher = [GTMHTTPFetcher fetcherWithRequest:request];
+    [fetcher beginFetchWithDelegate:self
+                  didFinishSelector:@selector(homeTimelineFetcher:finishedWithData:error:)];
+}
+
+// タイムライン (home_timeline) 取得応答時
+- (void)homeTimelineFetcher:(GTMHTTPFetcher *)fetcher
+           finishedWithData:(NSData *)data
+                      error:(NSError *)error
+{
+    if (error != nil) {
+        // タイムライン取得時エラー
+        NSLog(@"Fetching status/home_timeline error: %d", error.code);
+        return;
+    }
+    
+    // タイムライン取得成功
+    // JSONデータをパース
+    NSError *jsonError = nil;
+    NSArray *statuses = [NSJSONSerialization JSONObjectWithData:data
+                                                        options:0
+                                                          error:&jsonError];
+    
+    // JSONデータのパースエラー
+    if (statuses == nil) {
+        NSLog(@"JSON Parser error: %d", jsonError.code);
+        return;
+    }
+    
+    // データを保持
+    timelineStatuses_ = statuses;
+    
+    // テーブルを更新
+    [self.tableView reloadData];
 }
 
 #pragma mark - Table View
@@ -57,33 +194,109 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return _objects.count;
+    return [timelineStatuses_ count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
-
-    NSDate *object = _objects[indexPath.row];
-    cell.textLabel.text = [object description];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    
+    // 対象インデックスのステータス情報を取り出す
+    NSDictionary *status = [timelineStatuses_ objectAtIndex:indexPath.row];
+    
+    // ツイート本文を表示
+    cell.textLabel.numberOfLines = 0;
+    cell.textLabel.font = [UIFont systemFontOfSize:12];
+    cell.textLabel.text = [status objectForKey:@"text"];
+    
+    // ユーザ情報から screen_name を取り出して表示
+    NSDictionary *user = [status objectForKey:@"user"];
+    cell.detailTextLabel.text = [user objectForKey:@"screen_name"];
+    
     return cell;
 }
 
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+
+// 指定位置の行で使用する高さの要求
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+    // 対象インデックスのステータス情報を取り出す
+    NSDictionary *status = [timelineStatuses_ objectAtIndex:indexPath.row];
+    
+    // ツイート本文をもとにセルの高さを決定
+    NSString *content = [status objectForKey:@"text"];
+    CGSize labelSize = [content sizeWithFont:[UIFont systemFontOfSize:12]
+                           constrainedToSize:CGSizeMake(300, 1000)
+                               lineBreakMode:UILineBreakModeWordWrap];
+    return labelSize.height + 25;
 }
 
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+// ツイート投稿要求
+- (void)fetchPostTweet:(NSString *)text
 {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        [_objects removeObjectAtIndex:indexPath.row];
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+    // 要求を準備
+    NSURL *url = [NSURL URLWithString:@"http://api.twitter.com/1.1/statuses/update.json"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    
+    // statusパラメータをURI符号化してbodyにセット
+    NSString *encodedText = [GTMOAuthAuthentication encodedOAuthParameterForString:text];
+    NSString *body = [NSString stringWithFormat:@"status=%@", encodedText];
+    [request setHTTPBody:[body dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    // 要求に署名情報を付加
+    [auth_ authorizeRequest:request];
+    
+    // 接続開始
+    GTMHTTPFetcher *fetcher = [GTMHTTPFetcher fetcherWithRequest:request];
+    [fetcher beginFetchWithDelegate:self
+                  didFinishSelector:@selector(postTweetFetcher:finishedWithData:error:)];
+}
+
+// ツイート投稿要求に対する応答
+- (void)postTweetFetcher:(GTMHTTPFetcher *)fetcher finishedWithData:(NSData *)data error:(NSError *)error
+{
+    if (error != nil) {
+        // ツイート投稿取得エラー
+        NSLog(@"Fetching statuses/update error: %d", error.code);
+        return;
+    }
+    
+    // タイムライン更新
+    [self fetchGetHomeTimeline];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([[segue identifier] isEqualToString:@"showTweetView"]) {
+        [segue.destinationViewController setDelegate:self];
     }
 }
+
+// TweetViewでCancelが押された
+- (void)tweetViewControllerDidCancel:(TweetViewController *)viewController
+{
+    // TweetViewを閉じる
+    [viewController dismissModalViewControllerAnimated:YES];
+}
+
+// TweetViewでDoneが押された
+-(void)tweetViewControllerDidFinish:(TweetViewController *)viewController
+                            content:(NSString *)content
+{
+    // ツイートを投稿する
+    if ([content length] > 0) {
+        [self fetchPostTweet:content];
+    }
+    
+    // TweetViewを閉じる
+    [viewController dismissModalViewControllerAnimated:YES];
+    
+}
+
+
+
 
 /*
 // Override to support rearranging the table view.
@@ -101,13 +314,6 @@
 }
 */
 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    if ([[segue identifier] isEqualToString:@"showDetail"]) {
-        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        NSDate *object = _objects[indexPath.row];
-        [[segue destinationViewController] setDetailItem:object];
-    }
-}
+
 
 @end
